b89c80e87eee721feda67d96872dea27986216f2
Eduard Urbach <e.urbach@gmail.com>
2019-08-26 15:55:46 +0900

    Use akyoto/assert for tests

--- go.mod
+++ go.mod
@@ -7,6 +7,5 @@ require (
 	github.com/aerogo/http v1.0.9
 	github.com/akyoto/assert v0.2.0
 	github.com/json-iterator/go v1.1.7
-	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 )
--- parser/Character_test.go
+++ parser/Character_test.go
@@ -5,18 +5,16 @@ import (
 	"testing"
 
 	"github.com/aerogo/http/client"
-
-	"github.com/animenotifier/mal/parser"
-
-	"github.com/stretchr/testify/assert"
+	"github.com/akyoto/assert"
+	malparser "github.com/animenotifier/mal/parser"
 )
 
 func TestParseCharacter(t *testing.T) {
 	response, err := client.Get("https://myanimelist.net/character/63").End()
-	assert.NoError(t, err)
+	assert.Nil(t, err)
 
 	character, err := malparser.ParseCharacter(bytes.NewReader(response.Bytes()))
-	assert.NoError(t, err)
+	assert.Nil(t, err)
 	assert.Equal(t, "Winry Rockbell", character.Name)
 	// stringutils.PrettyPrint(character)
 }
